3690002375525f30565da8346ab9636bd8d5290f
JoyceMatos <j.matosjoyce@gmail.com>
2017-04-04 14:56:16 -0400

    Work on downloading image

---
+++ PhotoAlbum/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  PhotoAlbum
+//
+//  Created by Joyce Matos on 4/4/17.
+//  Copyright © 2017 Joyce Matos. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+}
--- PhotoAlbum/ViewController.swift
+++ PhotoAlbum/ViewController.swift
@@ -11,18 +11,64 @@ import UIKit
 class ViewController: UIViewController {
     
     let store = AlbumDataStore.shared
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-      self.store.getPhotos { (photos) in
-       
-        print("Hey these are the photos I am printing: \(self.store.photos)")
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
+        
+        retrievePhotos()
+      
+    }
+    
+    func retrievePhotos() {
+        self.store.getPhotos { (photos) in
+            DispatchQueue.main.async {
+                print("reloading data")
+                self.collectionView.reloadData()
+            }
         }
+    }
+    
+    func downloadImage(from url: String) {
+        let url = URL(string: url)
+        
+        
     }
 
 
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return store.photos.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! CollectionViewCell
+        
+        let photo = store.photos[indexPath.item]
+        
+        cell.label.text = "\(photo.id)"
+        cell.backgroundColor = UIColor.blue
+        
+        
+        return cell
+    }
+    
+    
+    
+    
 }
 
